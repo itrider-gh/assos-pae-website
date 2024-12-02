0500d085cc3d0025bc0af443890576e296973d6c
itrider-gh <votre.email@example.com>
2024-12-02 14:34:16 +0100

    go back to http

--- back/index.js
+++ back/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const https = require('https');
+const http = require('http'); // Remplace https par http
 const fs = require('fs');
 const app = express();
 
@@ -19,7 +19,7 @@ const config = require('./config');
 
 // Options pour CORS (autoriser certaines origines)
 var corsOptions = {
-  origin: [config.hosts.front, 'https://192.168.1.55:3000'],
+  origin: [config.hosts.front, 'http://192.168.1.55:3000'], // Mettez HTTP à la place de HTTPS
   credentials: true, // Autorise l'envoi des cookies
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'], // Autorise les en-têtes spécifiques
@@ -34,8 +34,8 @@ app.use(cookieSession({
   name: 'session',
   keys: [session.cookieKey],
   maxAge: 24 * 60 * 60 * 1000, // 24 heures
-  secure: true, // Utilisez 'true' pour HTTPS afin de sécuriser le cookie
-  sameSite: 'None', // Important pour l'envoi des cookies via requêtes cross-origin
+  secure: false, // Désactivez secure pour HTTP
+  sameSite: 'Lax', // Ajustez pour HTTP
   httpOnly: true // Protège contre l'accès aux cookies via JavaScript côté client
 }));
 app.use(passport.initialize());
@@ -52,24 +52,18 @@ app.use((err, req, res, next) => {
   handleError(err, res);
 });
 
-// Chargement des certificats SSL (Let's Encrypt ou autre)
-const httpsOptions = {
-  key: fs.readFileSync('selfsigned.key'),   // Clé privée
-  cert: fs.readFileSync('selfsigned.crt') // Certificat SSL
-};
-
 // Suppression du socket si déjà existant
 if (fs.existsSync(config.app.listenOn)) {
   console.warn('WARN: Unlink socket, is another instance running?');
   fs.unlinkSync(config.app.listenOn);
 }
 
-// Création du serveur HTTPS
-https.createServer(httpsOptions, app).listen(config.app.listenOn, () => {
+// Création du serveur HTTP
+http.createServer(app).listen(config.app.listenOn, () => {
   // Définir les permissions sur le socket (si nécessaire)
   if (fs.existsSync(config.app.listenOn)) {
     console.log('Set permissions on socket');
     fs.chmodSync(config.app.listenOn, '775');
   }
-  console.log('Listening on HTTPS ' + config.app.listenOn);
+  console.log('Listening on HTTP ' + config.app.listenOn);
 });
