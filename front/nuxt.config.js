70f7417eec519c31de641a634a07630bd53a04c5
itrider-gh <83413926+itrider-gh@users.noreply.github.com>
2024-12-11 14:26:00 +0100

    fix config

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,8 +1,8 @@
 // import colors from 'vuetify/es5/util/colors'
 import fr from 'vuetify/es5/locale/fr'
 
-let prod = process.env.NODE_ENV === 'dev'
-prod = false
+let prod = process.env.NODE_ENV === 'prod'
+prod = true
 
 export default {
   router: {
